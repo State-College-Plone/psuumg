899983d0c44578de7679fb9bbe784935e9a3038d
Craig Haynal <cah190@psu.edu>
2011-04-25 15:21:42 -0400

    ZMI UI can list, add, update.

--- Products/psuumg/plugin.py
+++ Products/psuumg/plugin.py
@@ -6,6 +6,7 @@ from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Products.PlonePAS.interfaces.group import IGroupManagement, IGroupIntrospection
 from Products.PlonePAS.interfaces.capabilities import IGroupCapability, IDeleteCapability
 from Products.PluggableAuthService.interfaces.plugins import IGroupEnumerationPlugin, IGroupsPlugin
+from Products.PluggableAuthService.permissions import ManageGroups
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 
 
@@ -65,47 +66,79 @@ class PSUUMGGroupManager(BasePlugin):
             group_filter = _PSUUMGGroupFilter(id, title, **kw)            
             
         for group_id in group_ids:
+            print group_id
             if self._groups.get(group_id, None):
                 url = '%s/manage_groups' % self.getId()
                 gid = 'group_id=%s' % group_id
+                info = {}
+                info.update(self._groups[group_id])
+                info['pluginid'] = plugin_id
+                info['properties_url'] = '%s?%s' % (url, gid)
+                info['members_url'] = '%s%s' % (self.prefix, info['id'])
+                
+                if not group_filter or group_filter(info):
+                    group_info.append(info)
         
         return tuple(group_info)
     
     # IGroupsPlugin
     security.declarePrivate('getGroupsForPrincipal')
     def getGroupsForPrincipal(self, principal, request=None):
-        return ()
+        # XXX: Test implementation
+        return ('umgtest',)
     
     # IGroupIntrospection
-    def getGroupById(group_id):
-        pass
+    def getGroupById(self, group_id, default=None):
+        plugins = self._getPAS()._getOb('plugins')
+        title = None
+        if group_id not in self.getGroupIds():
+            return default
+        
+        return self._findGroup(plugins, group_id, title)
     
-    def getGroups():
-        pass
+    def getGroups(self):
+        return map(self.getGroupById, self.getGroupIds())
     
-    def getGroupIds():
-        pass
+    def getGroupIds(self):
+        return self._groups.keys()
     
-    def getGroupMembers(group_id):
-        pass
+    def getGroupMembers(self, group_id):
+        # XXX: Test implementation
+        return ()
     
     # IGroupManagement
-    def addGroup(id, **kw):
-        pass
+    security.declarePrivate(ManageGroups, 'addGroup')
+    def addGroup(self, group_id, title=None, description=None, dn=None, **kw):
+        if self._groups.get(group_id) is not None:
+            raise KeyError, 'Duplicate group id: %s' % group_id
         
+        self._groups[group_id] = { 'id': group_id, 'dn': dn, 'title': title, 'description': description }
+        
+    security.declareProtected(ManageGroups, 'addPrincipalToGroup')
     def addPrincipalToGroup(self, principal_id, group_id):
         return False
     
-    def updateGroup(id, **kw):
-        pass
+    security.declarePrivate(ManageGroups, 'updateGroup')
+    def updateGroup(self, group_id, title=None, description=None, dn=None, **kw):
+        group = self._groups[group_id]
+        if dn is not None:
+            group['dn'] = dn
+        if title is not None:
+            group['title'] = title
+        if description is not None:
+            group['description'] = description
+        self._groups[group_id] = group 
         
-    def setRolesForGroup(group_id, roles=()):
+    security.declarePrivate(ManageGroups, 'setRolesForGroup')
+    def setRolesForGroup(self, group_id, roles=()):
         pass
     
-    def removeGroup(group_id):
+    security.declareProtected(ManageGroups, 'removeGroup')
+    def removeGroup(self, group_id, **kw):
         pass
     
-    def removePrincipalFromGroup(principal_id, group_id):
+    security.declareProtected(ManageGroups, 'removePrincipalFromGroup')
+    def removePrincipalFromGroup(self, principal_id, group_id):
         return False
     
     # IGroupCapability
@@ -121,6 +154,111 @@ class PSUUMGGroupManager(BasePlugin):
         if self._groups.get(principal_id) is not None:
             return 1
         return 0
+    
+    manage_options = ( ( { 'label': 'Groups', 
+                           'action': 'manage_groups', }
+                         ,
+                       )
+                     + BasePlugin.manage_options
+                     )
+
+    security.declareProtected( ManageGroups, 'manage_groups' )
+    manage_groups = PageTemplateFile( 'www/psuumgGroups'
+                                    , globals()
+                                    , __name__='manage_groups'
+                                    )
+    
+    security.declareProtected( ManageGroups, 'manage_addUMG' )
+    def manage_addUMG( self
+                     , group_id
+                     , dn
+                     , title=None
+                     , description=None
+                     , RESPONSE=None
+                     ):
+        """ Add a group via the ZMI.
+        """
+        self.addGroup( group_id, title, description, dn )
+
+        message = 'Group+added'
+
+        if RESPONSE is not None:
+            RESPONSE.redirect( '%s/manage_groups?manage_tabs_message=%s'
+                             % ( self.absolute_url(), message )
+                             )
+
+    security.declareProtected( ManageGroups, 'manage_updateUMG' )
+    def manage_updateUMG( self
+                          , group_id
+                          , dn
+                          , title
+                          , description
+                          , RESPONSE=None
+                          ):
+        """ Update a group via the ZMI.
+        """
+        self.updateGroup( group_id, title, description, dn )
+
+        message = 'Group+updated'
+
+        if RESPONSE is not None:
+            RESPONSE.redirect( '%s/manage_groups?manage_tabs_message=%s'
+                             % ( self.absolute_url(), message )
+                             )
+
+    # this is borrowed from PlonePAS's group.py plugin
+    security.declarePrivate('_findGroup')
+    def _findGroup(self, plugins, group_id, title=None, request=None):
+        """ group_id -> decorated_group
+        This method based on PluggableAuthService._findGroup
+        """
+
+        view_name = '_findGroup-%s' % group_id
+        keywords = { 'group_id' : group_id
+                   , 'title' : title
+                   }
+
+        group = self._createGroup(plugins, group_id, title)
+
+        propfinders = plugins.listPlugins(IPropertiesPlugin)
+        for propfinder_id, propfinder in propfinders:
+
+            data = propfinder.getPropertiesForUser(group, request)
+            if data:
+                group.addPropertysheet(propfinder_id, data)
+
+        groups = self._getPAS()._getGroupsForPrincipal(group, request
+                                            , plugins=plugins)
+        group._addGroups(groups)
+
+        rolemakers = plugins.listPlugins(IRolesPlugin)
+
+        for rolemaker_id, rolemaker in rolemakers:
+            roles = rolemaker.getRolesForPrincipal(group, request)
+            if roles:
+                group._addRoles(roles)
+
+        group._addRoles(['Authenticated'])
+
+        return group.__of__(self)
+    
+    security.declareProtected( ManageGroups, 'listGroupInfo' )
+    def listGroupInfo( self ):
+
+        """ -> ( {}, ...{} )
+
+        o Return one mapping per group, with the following keys:
+
+          - 'id' 
+        """
+        return self._groups.values()
+
+    security.declareProtected( ManageGroups, 'getGroupInfo' )
+    def getGroupInfo( self, group_id ):
+
+        """ group_id -> {}
+        """
+        return self._groups[ group_id ]
     
 # borrowed from Products.PluggableAuthService.plugins.ZODBGroupManager.py
 class _PSUUMGGroupFilter:
